5d59dd575ef84593dd694421ac79e18f8583bb89
Michael F. Herbst <info@michael-herbst.com>
2017-11-10 23:58:17 +0100

    Correct error in swaplines.sh script

--- 4_control_io/swaplines.sh
+++ 4_control_io/swaplines.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 read OLINE      # read the first line
-read LINE       # read the second line 
-echo "$OLINE"   # print second line
-echo "$LINE"    # print first line
+read LINE       # read the second line
+echo "$LINE"    # print second line
+echo "$OLINE"   # print first line
 cat
